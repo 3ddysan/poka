f12ebe3956090c9b82b285bee546362d3857f53c
3ddysan <3ddysan@users.noreply.github.com>
2022-04-23 16:02:06 +0200

    docs(storybook): add userlist story

---
+++ src/stories/UserList.stories.js
@@ -0,0 +1,37 @@
+import UserList from '../components/UserList.vue';
+
+export default {
+  title: 'UserList',
+  component: UserList,
+  argTypes: {
+    'show-results': { action: 'show-results' },
+    'reset-results': { action: 'reset-results' },
+    logout: { action: 'logout' },
+    users: {
+      control: {
+        type: 'array',
+      },
+      defaultValue: [
+        { name: 'Ted', voted: false },
+        { name: 'Joe', voted: true },
+        { name: 'Bob', voted: false },
+      ],
+    },
+  },
+};
+
+export const Playground = (args) => ({
+  components: { UserList },
+  setup() {
+    return { args };
+  },
+  template: `<UserList 
+      v-bind="args"
+      @show-results="args['show-results']"
+      @reset-results="args['reset-results']"
+      @logout="args.logout"
+    >
+    </UserList>`,
+});
+
+Playground.args = {};
