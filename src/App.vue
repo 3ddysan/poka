9accd1235ac4e47e38b32e5c41bb1234534519ed
3ddysan <3ddysan@users.noreply.github.com>
2022-07-13 14:32:35 +0200

    ref: handle routing by state inside App view
    - add transition effects

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,25 @@ import { useUserStore } from '@/stores/user';
 const user = useUserStore();
 const router = useRouter();
 
+router.beforeEach(async (to, from, next) => {
+  if (to.name === 'index') {
+    user.setName('');
+    next();
+    return;
+  }
+  if (user.connected) {
+    next();
+  } else {
+    next('/');
+  }
+});
+
 watch(
   () => user.connected,
   (connected) => {
     if (connected) {
       router.push('/board');
     } else {
-      user.reset();
       router.push('/');
     }
   },
@@ -18,5 +30,21 @@ watch(
 </script>
 
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,23 +1,10 @@
-import { useUserStore } from '@/stores/user';
 import { createRouter, createWebHistory } from 'vue-router';
 import routes from '~pages';
 
-const INDEX = { name: 'index' };
-routes.forEach((route) => {
-  if (route.name !== 'index') {
-    route.beforeEnter = (to, from, next) => {
-      if (useUserStore().connected) {
-        next();
-      } else {
-        next(INDEX);
-      }
-    };
-  }
-});
 routes.push({
   path: '/:pathMatch(.*)*',
   name: '404',
-  redirect: INDEX,
+  redirect: { name: 'index' },
 });
 
 export const router = createRouter({
