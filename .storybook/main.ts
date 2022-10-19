bf2e35e1ea7303562a28bfbb3cc41b449bf0e838
3ddysan <3ddysan@users.noreply.github.com>
2022-10-19 18:45:08 +0200

    build: dependency update
    - disable storybook docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -26,7 +26,12 @@ const config: StorybookViteConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false,
+      },
+    },
     '@storybook/addon-storysource',
   ],
   framework: '@storybook/vue3-vite',
