559b6f4a603c0daf32815dba38e179899fbc1d40
3ddysan <3ddysan@users.noreply.github.com>
2022-10-20 23:19:16 +0200

    fix(dev-server): close proxy connection on missing server

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig({
                 proxyRes.destroy();
               }
             });
+            proxyRes.on('close', () => {
+              res.destroy();
+            });
           });
         },
       },
