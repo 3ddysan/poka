e30b91b05ec099034cb65eb85e64cf4fd2d1873e
3ddysan <3ddysan@users.noreply.github.com>
2022-10-23 02:05:14 +0200

    feat: add poka logo
    - indicates everyone has voted

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { useStateStore } from '@/stores/state';
+
+const state = useStateStore();
 const { locale } = useI18n();
 const { language } = useNavigatorLanguage();
 const params = useUrlSearchParams('history');
@@ -13,6 +16,15 @@ watch(
 </script>
 
 <template>
+  <div class="absolute top-0 left-3">
+    <h1
+      :class="{ 'animate-bounce': state.mode === 'ready' }"
+      class="flag select-none tracking-3px uppercase box-content pt-2 w-8 h-14 flex items-center justify-center shadow-xl"
+    >
+      <span class="text-gray-300 write-vertical-right">Poka</span>
+      <div class="w-8 h-2 bg-gray-400 absolute top-15"></div>
+    </h1>
+  </div>
   <router-view v-slot="{ Component }">
     <transition name="fade" mode="out-in">
       <component :is="Component" />
@@ -29,5 +41,12 @@ watch(
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+
+.flag {
+  background: #32557f;
+  color: #7eb4e2;
+  font-size: 11px;
+  opacity: 0.9;
 }
 </style>
