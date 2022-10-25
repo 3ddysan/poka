fdb2d507ac463a62114f5717fcd8108349eb4383
3ddysan <3ddysan@users.noreply.github.com>
2022-10-25 20:10:38 +0200

    build: dependency update
    - cleanup storybook imports

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,6 @@
-import path from 'node:path';
-import { mergeConfig } from 'vite';
-import Components from 'unplugin-vue-components/vite';
-import AutoImport from 'unplugin-auto-import/vite';
-import WindiCSS from 'vite-plugin-windicss';
-import type { StorybookViteConfig } from '@storybook/builder-vite';
+import type { StorybookConfig } from '@storybook/builder-vite';
 
-const config: StorybookViteConfig = {
+const config: StorybookConfig = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
