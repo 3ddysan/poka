635af2bcc415afd92bd407cd27adf2bac52f9e1f
3ddysan <3ddysan@users.noreply.github.com>
2023-12-29 18:43:10 +0100

    ops: dependency update

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -12,7 +12,7 @@ defineProps({
     :class="{ 'animate-bounce': animate }"
     class="logo box-content h-14 w-8 flex select-none items-center justify-center bg-gray-400 bg-opacity-90 pt-2 text-[11px] tracking-3px uppercase shadow-xl dark:bg-[#393a59]"
   >
-    <span class="font-bold text-white write-vertical-right dark:text-[#f8f8f3]"
+    <span class="text-white font-bold write-vertical-right dark:text-[#f8f8f3]"
       >Poka</span
     >
     <div
